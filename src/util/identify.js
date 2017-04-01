b76176ea4841166aada9f6acce753ee6ef15b50c
trent <tashburn@gmail.com>
2017-04-01 16:36:36 -0600

    fixed exports in identify

--- src/util/identify.js
+++ src/util/identify.js
@@ -1,26 +1,28 @@
 
-const exports = module.exports = {}
+module.exports = {
 
-exports.isObject = (o) => { return typeof o === 'object' && !isArray(o) } 
+  isObject: (o) => { return typeof o === 'object' && !isArray(o) },
 
-exports.isArray = (o) => { return o.constructor === Array }
+  isArray: (o) => { return o.constructor === Array },
 
-exports.isFunction = (o) => { return typeof o === 'function' }
+  isFunction: (o) => { return typeof o === 'function' },
 
-exports.isBoolean = (o) => { return typeof o === 'boolean' }
+  isBoolean: (o) => { return typeof o === 'boolean' },
 
-exports.isNumber = (o) => { return typeof o === 'number' }
+  isNumber: (o) => { return typeof o === 'number' },
 
-exports.isString = (o) => { return typeof o === 'string' }
+  isString: (o) => { return typeof o === 'string' },
 
-exports.isSymbol = (o) => { return typeof o === 'symbol' }
+  isSymbol: (o) => { return typeof o === 'symbol' },
 
-exports.isNull = (o) => { return o === null }
+  isNull: (o) => { return o === null },
 
-exports.isDefined = (o) => { return typeof o !== 'undefined' }
+  isDefined: (o) => { return typeof o !== 'undefined' },
 
-exports.isUndefined = (o) => { return typeof o === 'undefined' }
+  isUndefined: (o) => { return typeof o === 'undefined' },
 
-exports.exists = (o) => { return isDefined(o) && !isNull(o) }
+  exists: (o) => { return isDefined(o) && !isNull(o) },
 
-exports.isCharNumber = (ch) => { return isString(ch) && ch.length==1 && /([0-9])/.test(ch) }
+  isCharNumber: (ch) => { return isString(ch) && ch.length==1 && /([0-9])/.test(ch) },
+
+}
