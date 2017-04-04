a68e0900e6af0e933b24bdf47b5c02abcbde7e54
trent <tashburn@gmail.com>
2017-04-03 18:48:10 -0600

    fixed isArray being available in utils/identify

--- src/util/identify.js
+++ src/util/identify.js
@@ -1,28 +1,40 @@
 
+function isObject(o) { return typeof o === 'object' && !isArray(o) }
+
+function isArray(o) { return o.constructor === Array }
+
+function isFunction(o) { return typeof o === 'function' }
+
+function isBoolean(o) { return typeof o === 'boolean' }
+
+function isNumber(o) { return typeof o === 'number' }
+
+function isString(o) { return typeof o === 'string' }
+
+function isSymbol(o) { return typeof o === 'symbol' }
+
+function isNull(o) { return o === null }
+
+function isDefined(o) { return typeof o !== 'undefined' }
+
+function isUndefined(o) { return typeof o === 'undefined' }
+
+function exists(o) { return isDefined(o) && !isNull(o) }
+
+function isCharNumber(ch) { return isString(ch) && ch.length==1 && /([0-9])/.test(ch) }
+
+
 module.exports = {
-
-  isObject: (o) => { return typeof o === 'object' && !isArray(o) },
-
-  isArray: (o) => { return o.constructor === Array },
-
-  isFunction: (o) => { return typeof o === 'function' },
-
-  isBoolean: (o) => { return typeof o === 'boolean' },
-
-  isNumber: (o) => { return typeof o === 'number' },
-
-  isString: (o) => { return typeof o === 'string' },
-
-  isSymbol: (o) => { return typeof o === 'symbol' },
-
-  isNull: (o) => { return o === null },
-
-  isDefined: (o) => { return typeof o !== 'undefined' },
-
-  isUndefined: (o) => { return typeof o === 'undefined' },
-
-  exists: (o) => { return isDefined(o) && !isNull(o) },
-
-  isCharNumber: (ch) => { return isString(ch) && ch.length==1 && /([0-9])/.test(ch) },
-
+  isObject: isObject,
+  isArray: isArray,
+  isFunction: isFunction,
+  isBoolean: isBoolean,
+  isNumber: isNumber,
+  isString: isString,
+  isSymbol: isSymbol,
+  isNull: isNull,
+  isDefined: isDefined,
+  isUndefined: isUndefined,
+  exists: exists,
+  isCharNumber: isCharNumber,
 }
